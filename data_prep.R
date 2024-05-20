4b79ed232bf4a7743bb61e62919e2af1448bf14c
Rohit Kandala <59447958+rok12003@users.noreply.github.com>
2024-05-20 11:58:15 -0500

    made some edits

--- data_prep.R
+++ data_prep.R
@@ -1,6 +1,8 @@
-# Importing packages
+# Importing packages:
 library(tidyverse)
 library(igraph)
+library(intergraph)
+library(statnet)
 
 # Preprocessing function that applies to columns and contents:
 preprocess_df <- function(df) {
@@ -23,12 +25,18 @@ org_df <- read.csv("Affiliation Matrix - Orgs.csv")
 education_df <- read.csv("Affiliation Matrix - Education.csv")
 ceo_attr_df <- read.csv("Affiliation Matrix - CEO_Attr.csv")
 org_attr_df <- read.csv("Affiliation Matrix - Org_Attr.csv")
+educ_attr_df <- read.csv("Affiliation Matrix - Ed_Attr.csv")
+
+# Converting to igraph object:
+gal_matrix <- as.matrix(CEOs)
+gal_1978 <- graph_from_incidence_matrix(gal_matrix, directed = FALSE)
 
 # Applying the preprocessing function:
 org_df <- preprocess_df(org_df)
 education_df <- preprocess_df(education_df)
 ceo_attr_df <- preprocess_df(ceo_attr_df)
 org_attr_df <- preprocess_df(org_attr_df)
+educ_attr_df <- preprocess_df(educ_attr_df)
 
 # Set the first column as row names & remove it from the data frame:
 # For orgs:
@@ -75,9 +83,26 @@ add_org_attributes <- function(graph, attr_df) {
   return(graph)
 }
 
+# Function to add organization attributes:
+add_educ_attributes <- function(graph, attr_df) {
+  for (col in colnames(attr_df)[-1]) {  # Exclude 'university' column
+    for (i in 1:nrow(attr_df)) {
+      university_name <- attr_df$university[i]
+      if (university_name %in% V(graph)$name) {
+        graph <- set_vertex_attr(graph, name = col, index = V(
+          graph)[name == university_name], value = attr_df[[col]][i])
+      }
+    }
+  }
+  return(graph)
+}
+
 # Adding CEO attributes to both networks:
 org_net <- add_ceo_attributes(org_net, ceo_attr_df)
 education_net <- add_ceo_attributes(education_net, ceo_attr_df)
 
 # Adding org attributes to org_net only:
 org_net <- add_org_attributes(org_net, org_attr_df)
+
+# Adding education attributes to education_net only:
+education_net <- add_educ_attributes(education_net, educ_attr_df)
